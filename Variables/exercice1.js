24677cfe1223d011a82f76dcadc1f32310f56ad5
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-04-01 14:51:50 +0200

    declaration variables

---
+++ Variables/exercice1.js
@@ -0,0 +1,7 @@
+var p1 = 10;
+const p2;
+var moyenne = calculMoyenne();
+console.log("P1 :"+p1+ " p2 : "+p2+ " P3 : "+p3);
+console.log(" La somme est de :"+somme);
+console.log(" La Moyenne est de :"+moyenne);
+
