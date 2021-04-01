3ab827d449003c9759bf1de027ae0986922b198c
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-04-01 15:46:35 +0200

    correction function

--- Variables/exercice1.js
+++ Variables/exercice1.js
@@ -1,7 +1,20 @@
-var p1 = 10;
-const p2;
-var moyenne = calculMoyenne();
+let p1 = 10;
+let p2;
+let p3
+let somme;
+let moyenne = calculMoyenne();
 console.log("P1 :"+p1+ " p2 : "+p2+ " P3 : "+p3);
 console.log(" La somme est de :"+somme);
 console.log(" La Moyenne est de :"+moyenne);
 
+/***
+ * Declaration de function
+ */
+function calculMoyenne(){
+    p1= p1+5;
+    p2 =6
+    p3 = 12
+     somme = p1+p2+ p3;
+    const result = somme / 3;
+     return result;
+}
